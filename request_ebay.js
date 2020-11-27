7c2bb4063af293b43c6eb70fa8a5386d0213b6f7
Rafael Hernandez <ralph@codingscool.com>
2020-11-26 22:25:13 -0500

    Testing the fetch code on google.com

--- request_ebay.js
+++ request_ebay.js
@@ -1,6 +1,11 @@
+const url = "https://www.google.com/"
+
 fetch(url)
 .then((response) => {
-    return response;
+    return response.text();
+})
+.then((html) => {
+    console.log(html)
 })
 .catch((error) => {
     console.log(error);
