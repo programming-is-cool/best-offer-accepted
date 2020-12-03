e31e0a4cd770001b318c62d00a684a379c6b408d
Rafael Hernandez <ralph@codingscool.com>
2020-12-02 22:39:32 -0500

    Functionality added to gather listing URLs.

--- contentScript.js
+++ contentScript.js
@@ -1,12 +1,34 @@
-const saleInfo = document.getElementsByClassName('s-item__purchase-options-with-icon');
+function getUrl(listingElement) {
+    let anchorElement = listingElement.getElementsByClassName('s-item__link');
+    if(anchorElement.length == 1){
+        return anchorElement[0].getAttribute('href');
+    }
+    return null;
+}
 
-let listingInfoArray = [];
+function listingRequest(url) {
+    var soldUrl = url + "&orig_cvip=true"
+    fetch(soldUrl)
+    .then((resp) => {
+        return resp.text();
+    })
+    .then((html) => {
+        console.log(html);
+    })
+    .catch((error) => {
+        console.log(error)
+    });
+}
+
+const saleInfo = document.getElementsByClassName('s-item__purchase-options-with-icon');
+let urlList = []
 
 for(i=0; i < saleInfo.length; i++) {
     if(saleInfo[i].innerHTML ==  'Best offer accepted') {
         let parentElement = saleInfo[i].closest('.s-item__info');
-        listingInfoArray.push(parentElement);
+        let returnedUrl = getUrl(parentElement);
+        urlList.push(returnedUrl);
     }
 }
 
-console.log(listingInfoArray);
+console.log(urlList);
