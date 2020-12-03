6b3e80842d60c891ccc890b2d6d19bc211d88f6c
Rafael Hernandez <ralph@codingscool.com>
2020-12-02 22:47:46 -0500

    Added a quick test in case of a missing URL.

--- contentScript.js
+++ contentScript.js
@@ -27,6 +27,11 @@ for(i=0; i < saleInfo.length; i++) {
     if(saleInfo[i].innerHTML ==  'Best offer accepted') {
         let parentElement = saleInfo[i].closest('.s-item__info');
         let returnedUrl = getUrl(parentElement);
+        if (returnedUrl == null) {
+            console.log("The following element returned a null URL value:")
+            console.log(parentElement.innerHTML)
+            continue;
+        }
         urlList.push(returnedUrl);
     }
 }
